c295c92007c1d789d501dd404c8606a451249847
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-31 08:29:28 -0500

    Retirar gestión de PIN de coordinadores

--- app/admin-shell.tsx
+++ app/admin-shell.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable @next/next/no-img-element */
 
 import { FormEvent, useState } from "react";
-import CoordinatorPinAdmin from "./coordinator-pin-admin";
 import SiacdApp from "./siacd-app-v6";
 
 const ADMIN_PIN_HASH = "e6955a2c59dc90833986fe0894cf6718dddaa7816bb51bc955cdd3eb4470e554";
@@ -41,7 +40,7 @@ export default function AdminShell() {
     setPin("");
   }
 
-  if (authorized) return <><SiacdApp forcedAccess="admin" /><CoordinatorPinAdmin /></>;
+  if (authorized) return <SiacdApp forcedAccess="admin" />;
 
   return (
     <div className="login-page">
